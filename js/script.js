a9750f877485fbf721ce701ae128563eeef584e1
John <john.njoroge@student.moringaschool.com>
2022-03-11 11:22:18 +0300

    feat:navbar fixing

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// var sum =0;
+// // if ((x < 50) || ((x%2)==0)){
+// //     console.log("data is valid");
+// // } else if (x < 0){
+// //     console.log("data is invalid");
+// // };
+
+// for (var i = 1; i <= 10; i = i + 5){
+//     sum = sum +i;
+// }
+
+// console.log(sum);
+
+// const gifts = ["voucher","tshirt", "flask"]
+// var i = gifts.length-1;
+// while (i > 0){
+//     console.log("wrapped" +gifts[i]);
+//     i--;
+// }
+
+var x = 100;
+
+while (x < 200){
+    x++;
+}
+
+console.log(x);
+
+var x = 100;
+
+while (x < 200){
+    x--;
+}
+
+console.log(x);
+
+var x = 100;
+
+while (x > 0){
+    x--;
+}
+
+console.log(x);
